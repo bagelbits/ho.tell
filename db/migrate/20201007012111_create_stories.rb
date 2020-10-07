dadc8a5afe9738105fdc6da885ffa6148fa19480
Christopher Ward <cdurianward@gmail.com>
2020-10-06 19:09:23 -0700

    Update the migration file to reflect reality.

--- db/migrate/20201007012111_create_stories.rb
+++ db/migrate/20201007012111_create_stories.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class CreateStories < ActiveRecord::Migration[6.0]
   def change
     create_table :stories do |t|
-      t.text :story
-      t.boolean :reported
+      t.text :story, null: false
+      t.boolean :reported, default: false
 
       t.timestamps
     end
