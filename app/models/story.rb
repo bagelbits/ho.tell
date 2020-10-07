3e53dbd5db8a4be0aaea6d6f33d1cb42906af316
Christopher Ward <cdurianward@gmail.com>
2020-10-07 02:24:32 -0700

    Cread Story.random

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 class Story < ApplicationRecord
+  class << self
+    def random(ignored_ids = [])
+      results = if ignored_ids.present?
+                  where.not(id: ignored_ids)
+                else
+                  all
+                end
+      results.order(Arel.sql('RAND()')).first
+    end
+  end
+
   validates :story, presence: true
 
   def report!
--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -3,14 +3,35 @@
 require 'rails_helper'
 
 RSpec.describe Story, type: :model do
-  describe '#report' do
-    it 'marks prompt as reported' do
-      prompt = Story.create(story: 'This is a saucy story')
-      expect(prompt.reported).to eq(false)
+  describe ':random' do
+    let!(:story1) { Story.create(story: 'This is a saucy story') }
+    let!(:story2) { Story.create(story: 'This is a saucy story') }
+    let!(:story3) { Story.create(story: 'This is a saucy story') }
+    let!(:story_ids) { [story1, story2, story3].map(&:id) }
 
-      prompt.report!
-      prompt = Story.find(prompt.id)
-      expect(prompt.reported).to eq(true)
+    it 'selects a random story' do
+      random_story = Story.random
+
+      expect(story_ids).to include(random_story.id)
+    end
+
+    context 'with ignored_ids' do
+      it 'selects a random story' do
+        random_story = Story.random([story1.id, story3.id])
+
+        expect(random_story.id).to eq(story2.id)
+      end
+    end
+  end
+
+  describe '#report' do
+    it 'marks story as reported' do
+      story = Story.create(story: 'This is a saucy story')
+      expect(story.reported).to eq(false)
+
+      story.report!
+      story = Story.find(story.id)
+      expect(story.reported).to eq(true)
     end
   end
 end
