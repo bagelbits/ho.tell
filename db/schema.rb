3958684a103139d1b7852d52b089ebfc49a8b86b
Christopher Ward <cdurianward@gmail.com>
2020-10-07 00:44:41 -0700

    Will that fix it?

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_10_07_012111) do
+
+  create_table "stories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.text "story", null: false
+    t.boolean "reported", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
