a1072f6ea2ef8bafaa2f048498ccd4f49bde4add
Christopher Ward <cdurianward@gmail.com>
2021-01-12 12:33:49 -0800

    Other layout changes

--- app/javascript/components/Admin.jsx
+++ app/javascript/components/Admin.jsx
@@ -9,17 +9,19 @@ function Index() {
   return (
     <div>
       <h1 className="title">Ho.tell Admindesk</h1>
-      <Tabs defaultActiveKey="toReview" unmountOnExit id="uncontrolled-tab-example">
-        <Tab eventKey="toReview" title="To Review">
-          <ToReviewStories />
-        </Tab>
-        <Tab eventKey="approved" title="Approved">
-          <ApprovedStories />
-        </Tab>
-        <Tab eventKey="reported" title="Reported">
-          <ReportedStories />
-        </Tab>
-      </Tabs>
+      <div className="story-tabs">
+        <Tabs defaultActiveKey="toReview" unmountOnExit id="uncontrolled-tab-example">
+          <Tab eventKey="toReview" title="To Review">
+            <ToReviewStories />
+          </Tab>
+          <Tab eventKey="approved" title="Approved">
+            <ApprovedStories />
+          </Tab>
+          <Tab eventKey="reported" title="Reported">
+            <ReportedStories />
+          </Tab>
+        </Tabs>
+      </div>
     </div>
   );
 }
