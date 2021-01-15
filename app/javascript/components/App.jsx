89f60684d30331cb4e5ba9ded9e5d4f763b2c4ea
Christopher Ward <cdurianward@gmail.com>
2021-01-14 17:41:18 -0800

    Styling changes

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -6,9 +6,17 @@ function Index() {
   return (
     <div>
       <h1 className="title">Welcome to Ho | Tell</h1>
-      <p>We are a digital gossip exchange service for your needs to know and share your dirty, wicked, strange, and wild confessions with. We don’t track your identity, our greatest hope is that you are telling the truth.</p>
-      <p>Please keep submissions anonymous, and follow our community safety guidelines.</p>
-      <p>We are excited to share secrets with you.</p>
+      <h2 className="instructions">
+        We are a digital gossip exchange service for your needs to know and share your dirty,
+        wicked, strange, and wild confessions with. We don’t track your identity, our greatest hope
+        is that you are telling the truth.
+        <br />
+        <br />
+        Please keep submissions anonymous, and follow our community safety guidelines.
+        <br />
+        <br />
+        We are excited to share secrets with you.
+      </h2>
       <video controls autobuffer autoPlay muted loop id="ho-tell-video" src={HoTellVideo} />
       <StoryPrompt />
 
